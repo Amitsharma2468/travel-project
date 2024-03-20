6356fb0ff4c5301e4128c2b73dc7c8eb4e022e17
Amitsharma2468 <amitkmrsharma292@gmail.com>
2024-03-20 15:30:50 +0600

    Initialy updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import BotImage from './dotor1.jpg'; // Import the bot image
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="title">
+          <span className="lightning-text">PULSE GEN</span>
+        </div>
+        <div className="container">
+          <div className="left-container">
+            <img src={BotImage} alt="Bot" className="bot-image" />
+          </div>
+          <div className="right-container">
+            <h1>
+              <span className="hello-text">HELLO,</span><span className="ai-text"> I AM your Doctor AI assistant.</span>
+            </h1>
+            <h2><span className="about-text">I am dedicated to empowering you with knowledge and tools
+             you need to take control of your health and well-being.</span></h2>
+             <button className="start-button">LET'S START</button>
+          </div>
+        </div>
       </header>
     </div>
   );
